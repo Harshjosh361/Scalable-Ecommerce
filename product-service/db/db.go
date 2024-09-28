5a44959c0d4c05b5b732aef576d66fc8a6ab1f6b
Harshjosh361 <harshjosh361@gmail.com>
2024-09-28 17:57:03 +0530

    ports modified

--- product-service/db/db.go
+++ product-service/db/db.go
@@ -10,11 +10,16 @@ import (
 )
 
 func Init() *mongo.Database {
-	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOption := options.Client().ApplyURI("mongodb://mongo:27018/")
 
 	client, err := mongo.Connect(context.Background(), clientOption)
 	if err != nil {
 		log.Fatal("Failed to connect to database")
+	}
+	// ping db
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("failed to ping db")
 	}
 	fmt.Println("Successfully connected to database")
 	return client.Database("Testing")
--- product-service/main.go
+++ product-service/main.go
@@ -22,7 +22,7 @@ func main() {
 	// routes
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Server is live",
+			"message": "Product Server is live",
 		})
 	})
 
